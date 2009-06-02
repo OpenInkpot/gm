46a0c3df1b95f52d79d0adea0286035f2a11f711
Alexander V. Nikolaev <avn@daemon.hole.ru>
2009-06-02 23:19:21 +0000

    Implement real controls

--- src/setup.c
+++ src/setup.c
@@ -6,6 +6,8 @@
 #include "setup.h"
 #include "lang.h"
 #include "choices.h"
+#include "sound_control.h"
+#include "screen_update_control.h"
 
 
 struct setup_menu_item_t {
@@ -16,24 +18,62 @@ struct setup_menu_item_t {
 };
 
 
+const char * screen_states[] = {
+    "N/A",
+    "Full",
+    "Adaptive",
+    "Partial"
+};
+
 static void
 screen_draw(Evas_Object *item)
 {
-    edje_object_part_text_set(item, "text", "Screen update : ON");
+    char *buf;
+    screen_update_t scr = detect_screen_update_type();
+    asprintf(&buf, "Screen update : %s", screen_states[scr+1]);
+    edje_object_part_text_set(item, "text", buf);
+    free(buf);
 }
 
 static void
-screen_set(Evas_Object * self __attribute__((unused))) {
+screen_set(Evas_Object * self) {
+    screen_update_t scr = detect_screen_update_type();
+    if(scr < 0)
+        return;
+
+    scr ++;
+    if(scr > SCREEN_UPDATE_PARTIAL)
+        scr = SCREEN_UPDATE_FULL;
+    printf("screen: %d\n", scr);
+        set_screen_update_type(scr);
+        choicebox_invalidate_item(self, 1);
 }
+
+
+const char * sound_states[] = {
+    "N/A",
+    "OFF",
+    "ON"
+};
 
 static void
 sound_draw(Evas_Object *item)
 {
-    edje_object_part_text_set(item, "text", "Sound : ON");
+    char *buf;
+    sound_t snd = detect_sound();
+    asprintf(&buf, "Sound : %s", sound_states[snd+1]);
+    edje_object_part_text_set(item, "text", buf);
+    free(buf);
 }
 
 static void
-sound_set(Evas_Object * self __attribute__((unused))) {
+sound_set(Evas_Object * self) {
+    sound_t snd = detect_sound();
+    if(snd == SOUND_ON)
+       set_sound(SOUND_OFF);
+    else if (snd == SOUND_OFF)
+       set_sound(SOUND_OFF);
+    choicebox_invalidate_item(self, 2);
 }
 
 static void
@@ -45,17 +85,11 @@ language_draw(Evas_Object *item)
     free(buf);
 }
 
-static void
-language_set(Evas_Object * self) {
-   lang_menu(self);
-}
-
-
 #define MENU_ITEMS_NUM 3
 struct setup_menu_item_t setup_menu_items[] = {
     {"Screen update: %s", &screen_draw, &screen_set, 0},
     {"Sound: %s",  &sound_draw, &sound_set, 0},
-    {"Language: %s", &language_draw, &language_set, 0},
+    {"Language: %s", &language_draw, &lang_menu, 0},
     {NULL, NULL, NULL, NULL},
 };
 
